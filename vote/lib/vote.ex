e625918c7d224fe090ffad743c05a19b02e03fe2
Herminio Torres <herminiocesar@gmail.com>
2021-01-11 12:16:12 -0300

    add first elixir application

---
+++ vote/lib/vote.ex
@@ -0,0 +1,18 @@
+defmodule Vote do
+  @moduledoc """
+  Documentation for `Vote`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Vote.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
