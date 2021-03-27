b5b392d1f9da4a88267a1bdd33960b12857ca3be
Herminio Torres <herminiocesar@gmail.com>
2021-03-27 19:42:28 -0300

    add district changeset

--- exit_polling/poller_dal/lib/poller_dal/districts/district.ex
+++ exit_polling/poller_dal/lib/poller_dal/districts/district.ex
@@ -1,9 +1,17 @@
 defmodule PollerDal.Districts.District do
   use Ecto.Schema
+  import Ecto.Changeset
 
   schema "districts" do
     field(:name, :string)
 
     timestamps()
+  end
+
+  def changeset(%__MODULE__{} = struct, %{} = attrs) do
+    struct
+    |> cast(attrs, [:name])
+    |> validate_required([:name])
+    |> validate_length(:name, min: 2)
   end
 end
